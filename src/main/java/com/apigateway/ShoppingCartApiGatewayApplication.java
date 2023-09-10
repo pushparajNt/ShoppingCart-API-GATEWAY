69be5260c74952dfa075aa165d3b46f37ead677c
Pushparaj - <pushparaj.-@ibm.com>
2023-09-10 18:49:06 +0530

    first commit

---
+++ src/main/java/com/apigateway/ShoppingCartApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartApiGatewayApplication.class, args);
+	}
+
+}
